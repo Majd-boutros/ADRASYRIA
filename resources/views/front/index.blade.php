c7c05911716ad648a188c4ebd6be7d88eeb24e56
Majd-boutros <majdboutros25@gmail.com>
2021-04-27 14:28:05 +0300

    third commit

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -347,3 +347,4 @@
 
     </div>
 @endsection
+
--- resources/views/layouts/front_layout/footer.blade.php
+++ resources/views/layouts/front_layout/footer.blade.php
@@ -2,16 +2,17 @@
 <footer class="footer-99382">
 
     <div class="container">
-        <div class="row">
+        <div class="row text-center">
             <div class="col-md-4 pr-md-5">
                 <img class="footer-logo" src="{{asset('images/front/footer_logo/adra-vertical-logo_wht.png')}}" alt="adra logo">
+                <br><br>
                 <p>Adventist Development & Relief Agency (ADRA) Syria</p>
                 <address>
                     Syria
                     Damascus / Al Mezzeh -  Ebn Alwaleed St - House 64 Block 18
                 </address>
             </div>
-            <div class="col-md">
+            <div class="col-md text-center">
                 <h3>Discover</h3>
                 <ul class="list-unstyled nav-links">
                     <li><a href="#">Home</a></li>
@@ -21,7 +22,7 @@
                     <li><a href="#">Contact</a></li>
                 </ul>
             </div>
-            <div class="col-md">
+            <div class="col-md text-center">
                 <h3>About</h3>
                 <ul class="list-unstyled nav-links">
                     <li><a href="#">Clients</a></li>
@@ -31,7 +32,7 @@
                     <li><a href="#">Journal</a></li>
                 </ul>
             </div>
-            <div class="col-md">
+            <div class="col-md text-center">
                 <h3>Help</h3>
                 <ul class="list-unstyled nav-links">
                     <li><a href="#">Privacy Policy</a></li>
@@ -39,7 +40,7 @@
                     <li><a href="#">Partners</a></li>
                 </ul>
             </div>
-            <div class="col-md">
+            <div class="col-md text-center">
                 <h3>Follow Us</h3>
                 <ul class="social list-unstyled">
                     <li><a href="#" class="pl-0"><span class="icon-instagram"></span></a></li>
@@ -51,7 +52,7 @@
             </div>
         </div>
 
-        <div class="row ">
+        <div class="row">
             <div class="col-12 text-center">
                 <div class="copyright mt-5 pt-5">
                     <p><small>&copy; 2021&mdash;2022 All Rights Reserved.</small></p>
